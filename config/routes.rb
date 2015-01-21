28ed9e4f28dae8e5c55420f86012eb9a19b8c6eb
ethanp <ethanp@utexas.edu>
2015-01-21 00:21:28 -0600

    links to static pages in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  get 'visitors/about', as: :about
+  get 'visitors/contact', as: :contact
+  get 'visitors/join', as: :join
+  get 'visitors/rules', as: :rules
 end
